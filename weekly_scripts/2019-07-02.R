792f00753eb209eb4a856448d2f5c740cfe153ab
k8hertweck <k8hertweck@gmail.com>
2019-07-03 13:26:12 -0700

    entertainment franchises

---
+++ weekly_scripts/2019-07-02.R
@@ -0,0 +1,26 @@
+#### 26 Feb 2019 ####
+
+library(tidyverse)
+library(tidytuesdayR)
+
+# download this week's data
+tt_data<-tt_load("2019-07-02")
+print(tt_data)
+
+# extract data of interest
+media_franchises <- tt_data$media_franchises
+
+unique(media_franchises$franchise)
+unique(media_franchises$revenue_category)
+
+franchises <- media_franchises %>%
+  group_by(franchise, , revenue_category, year_created) %>%
+  summarise(total_rev = sum(revenue))
+
+#### Data Viz ####
+
+# time series
+ggplot(franchises, aes(x=year_created, 
+                         y=total_rev)) +
+  geom_line(aes(color = revenue_category))
+ggsave("figures/2019-07-02.jpg", width = 8, height = 4)
